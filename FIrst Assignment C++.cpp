6c0cc7f7ade6a51e84ddfc0fa0ab57ecbe6315c3
Arnd1921 <146384225+Arnd1921@users.noreply.github.com>
2023-09-30 15:37:42 +0300

    Add files via upload

---
+++ FIrst Assignment C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+  cout << "Hello! We want to output this text "<<endl; 
+  cout << "Hello! We want to output this text "<<endl;
+  cout << "Hello! We want to output this text "<<endl;
+  cout << "Hello! We want to output this text "<<endl;
+
+  return 0;
+}
