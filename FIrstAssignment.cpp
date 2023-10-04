85edd4ac895188118c981e3be9370d69327a8a0a
unknown <arsene.ndimanya@strathmore.edu>
2023-10-04 22:42:01 +0300

    basic output c++ program

--- FIrstAssignment.cpp
+++ FIrstAssignment.cpp
@@ -3,23 +3,9 @@
 using namespace std;
 
 int main() {
-  int num1, num2, num3, sum;
-
-  // Prompt the user to enter three numbers
-  cout << "Enter the first number: ";
-  cin >> num1;
-
-  cout << "Enter the second number: ";
-  cin >> num2;
-
-  cout << "Enter the third number: ";
-  cin >> num3;
-
-  // Calculate the sum of the three numbers
-  sum = num1 + num2 + num3;
-
-  // Output the sum to the user
-  cout << "The sum of the two numbers is: " << sum << endl;
+  cout << "Welcome to the wonderful world of C++!" << endl;
+  cout << "This is my first C++ output statement" << endl;
+  cout << "Soon, I will learn how to input data into my program" << endl;
 
   return 0;
 }
