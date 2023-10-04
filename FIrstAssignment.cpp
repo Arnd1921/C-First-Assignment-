b1d21cf322923ca26841683ddbb2d58f4f17f2e8
unknown <arsene.ndimanya@strathmore.edu>
2023-10-04 22:25:40 +0300

    added a third number

--- FIrstAssignment.cpp
+++ FIrstAssignment.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 
 int main() {
-  int num1, num2, sum;
+  int num1, num2, num3, sum;
 
-  // Prompt the user to enter two numbers
+  // Prompt the user to enter three numbers
   cout << "Enter the first number: ";
   cin >> num1;
 
   cout << "Enter the second number: ";
   cin >> num2;
 
-  // Calculate the sum of the two numbers
-  sum = num1 + num2;
+  cout << "Enter the third number: ";
+  cin >> num3;
+
+  // Calculate the sum of the three numbers
+  sum = num1 + num2 + num3;
 
   // Output the sum to the user
   cout << "The sum of the two numbers is: " << sum << endl;
