4de4d905fe1e457dd85ccc7611c0e2391016bc2c
unknown <arsene.ndimanya@strathmore.edu>
2023-10-04 22:11:52 +0300

    adding operation for summing

---
+++ FIrstAssignment.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num1, num2, sum;
+
+  // Prompt the user to enter two numbers
+  cout << "Enter the first number: ";
+  cin >> num1;
+
+  cout << "Enter the second number: ";
+  cin >> num2;
+
+  // Calculate the sum of the two numbers
+  sum = num1 + num2;
+
+  // Output the sum to the user
+  cout << "The sum of the two numbers is: " << sum << endl;
+
+  return 0;
+}
